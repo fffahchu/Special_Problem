1d0313d061f28a28de33d11d0a5751ef5d3f454a
fffahchu <89907093+fffahchu@users.noreply.github.com>
2023-03-21 23:27:20 +0700

    Upload

--- components/Modal.js
+++ components/Modal.js
@@ -1,46 +1,73 @@
 import React from "react";
-const Modal = ({ setOpenModal }) => {
+
+const Modal = ({ isVisible, onClose }) => {
+  if (!isVisible) return null;
+  const handleClose = (e) => {
+    if (e.target.id === "wrapper") onClose();
+  };
+
   return (
-    <div className="flex justify-center bg-white w-[834px] h-[488px]">
-      <div className="modalContainer">
-        <div className="titleCloseBtn">
-          <button
-            onClick={() => {
-              setOpenModal(false);
-            }}
-          >
-            X
-          </button>
-        </div>
-        <div className="title">
-          <h1>สร้างอัลบัม</h1>
-        </div>
-        <div>
-          <img
-            className="w-[100px] h-[100px]"
-            src="../assets/image/photo.png"
-          ></img>
-        </div>
-        <div className="w-[338px] h-[49px] border rounded-[40px]">
-          <input id="file_name" type="text" placeholder="ตั้งชื่ออัลบัม" />
-        </div>
-        <div className="footer">
-          <button
-            className="bg-[#FFCA61] w-[86px] h-[36px] rounded shadow shadow-m"
-            onClick={() => {
-              setOpenModal(false);
-            }}
-            id="cancelBtn"
-          >
-            บันทึก
-          </button>
-          <button className="bg-[#D9D9D9] w-[86px] h-[36px] rounded shadow shadow-m">
-            ยกเลิก
-          </button>
+    <div
+      className="flex justify-center items-center fixed inset-0 bg-black bg-opacity-25 bg-white backdrop-blur-lg"
+      id="wrapper"
+      onClick={handleClose}
+    >
+      <div className="flex">
+        <div className="flex-col">
+          <div className="relative">
+            <div className="bg-white w-[600px] h-[370px] shadow-lg rounded-lg">
+              <div className="absolute top-2 right-2">
+                <button
+                  onClick={() => {
+                    onClose();
+                  }}
+                >
+                  <img
+                    src="../assets/icons/close-circle.svg"
+                    alt="close-circle"
+                  />
+                </button>
+              </div>
+
+              <div className="font-semibold text-[23px] pt-[50px] pl-[250px]">
+                สร้างอัลบัม
+              </div>
+              <div className="pt-[10px] pl-[255px]">
+                <img
+                  className="w-[100px] h-[100px]"
+                  src="../assets/icons/album.svg"
+                  alt="album"
+                ></img>
+              </div>
+
+              <label
+                for="album_name"
+                className="pl-[132px] w-[338px] h-[49px] text-lg font-bold text-start"
+              ></label>
+              <input
+                className="mt-[20px] border rounded-2xl w-[338px] h-[49px] py-[6px] px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="album_name"
+                type="text"
+                placeholder="เช่น กิจกรรม 2/2565"
+              />
+
+              <div className="flex gap-4 absolute bottom-2 right-2">
+                <div>
+                  <button className="w-[101px] h-[42px] rounded-2xl shadow-md transition ease-in-out  bg-[#d4d4d4] hover:-translate-y-1 hover:scale-110 hover:bg-[#FFCA61] duration-300">
+                    บันทึก
+                  </button>
+                </div>
+                <div>
+                  <button className="w-[101px] h-[42px] rounded-2xl shadow-md transition ease-in-out  bg-[#d4d4d4] hover:-translate-y-1 hover:scale-110 hover:bg-[#FFCA61] duration-300">
+                    ยกเลิก
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default Modal;
--- pages/upload/index.js
+++ pages/upload/index.js
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React, { Component, Fragment, useState } from "react";
 import MoveToTop from "@components/MoveToTop";
 import Link from "next/link";
 import Breadcrumb from "@components/Breadcrumb";
-{
-  /*import Modal from "@Components/Modal";*/
-}
+import Modal from "@components/Modal";
 
 const Upload = () => {
   const route = [
@@ -18,37 +16,79 @@ const Upload = () => {
     },
   ];
 
-  const [modalOpen, setModalOpen] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    <div className="px-[104px] py-[29px]">
-      <MoveToTop />
-      <Breadcrumb route={route} />
-      <div className="text-center text-[28px] font-semibold my-12">
-        ผลงานของฉัน
-      </div>
-      <hr className="mt-[18px] mb-4" />
+    <Fragment>
+      <div className="px-[104px] py-[29px]">
+        <MoveToTop />
+        <Breadcrumb route={route} />
+        <div className="text-center text-[28px] font-semibold my-12">
+          ผลงานของฉัน
+        </div>
+        <hr className="mt-[18px] mb-4 border-[#d4d4d4]" />
 
-      <div className="flex">
-        <div className="flex flex-col">
-          <div className="flex justify-center items-center self-end mb-[20px] w-[321px] h-[427px] bg-white border border-[#D9D9D9] rounded-[10px]">
-            <img
-              src="/assets/icons/camera-icon.svg"
-              alt="/assets/icons/camera-icon.svg"
-            />
-          </div>
-          <div className="pt-[315px] pr-[56px]">
+        <div className="flex">
+          <div className="flex justify-center">
             <button
-              for="dropzone-file"
-              className="bg-[#FFCA61] w-[321px] h-[111px] rounded-b-lg text-[18px] font-bold cursor-pointer"
+              className="flex justify-center items-center bg-[#FFCA61] w-[212px] h-[57px] rounded-2xl shadow-md font-semibold"
+              onClick={() => setShowModal(true)}
             >
-              อัพโหลดรูปภาพ
+              <img src="../assets/icons/plus-circle.svg" alt="plus-circle" />
+              {""}เพิ่มอัลบัม
             </button>
+          </div>
+        </div>
+        <hr className="mt-[18px] mb-4 border-[#d4d4d4]" />
+
+        <div className="flex">
+          <div className="flex flex-col ml-[330px] mt-[40px]">
+            <div className="flex justify-center items-center self-end mb-[20px] w-[321px] h-[427px] border rounded-[6px]">
+              <img
+                src="/assets/icons/camera-icon.svg"
+                alt="/assets/icons/camera-icon.svg"
+              />
+            </div>
+            <div className="flex justify-center">
+              <div>
+                <button className="w-[101px] h-[42px] rounded-2xl shadow-md transition ease-in-out  bg-[#d4d4d4] hover:-translate-y-1 hover:scale-110 hover:bg-[#FFCA61] duration-300">
+                  บันทึก
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="ml-[70px]">
+            <div className="flex flex-col mb-6 mt-[80px]">
+              <label
+                for="file_name"
+                className="w-[278px] text-lg font-bold pt-[60px] text-start"
+              >
+                ชื่อไฟล์
+              </label>
+              <input
+                className="mt-[8px] shadow appearance-none border rounded-lg w-[278px] py-[6px] px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="file_name"
+                type="text"
+                placeholder="เช่น ผลการเรียน 2/2565"
+              />
+            </div>
+            <label
+              for="dropzone-file"
+              className="px-[20px] py-[6px] w-[165px] border-[1px] rounded-[20px] font-bold cursor-pointer"
+            >
+              อัพโหลดรูป
+            </label>
             <input id="dropzone-file" type="file" className="hidden" />
           </div>
         </div>
+        <div className="flex justify-between items-center mt-[80px]">
+          <div className="text-[20px] font-semibold">ผลงานของฉันทั้งหมด</div>
+        </div>
+        <hr className="mt-[12px] mb-4 border-[#d4d4d4]" />
       </div>
-    </div>
+      <Modal isVisible={showModal} onClose={() => setShowModal(false)}></Modal>
+    </Fragment>
   );
 };
 export default Upload;
