c32c1c831e82eaa7f4d2c20b4a76798ca938805a
Faris Hayimasalae <faris.hayimasalae@gmail.com>
2023-04-16 18:00:47 +0700

    header dropdown

--- components/Header.js
+++ components/Header.js
@@ -1,46 +1,175 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Link from "next/link";
 
 const Header = () => {
+  const [showDropdown, setShowDropdown] = useState("");
+  const menuRef = useRef();
   const menu = [
-    { title: "หน้าหลัก", icon: "home", location: "/" },
+    { title: "หน้าหลัก", icon: "home", location: "/member" },
     { title: "ข่าวประชาสัมพันธ์", icon: "advertise", location: "/" },
     { title: "แนะนำคณะ", icon: "advice", location: "/" },
     { title: "คลังความรู้", icon: "book", location: "/" },
   ];
 
+  const advertises = [
+    { title: "ทั้งหมด", location: "/" },
+    { title: "ข่าวสาร", location: "/" },
+    { title: "กิจกรรม/ค่าย", location: "/" },
+    { title: "บทความ", location: "/" },
+  ];
+
+  const advices = [
+    { title: "ทั้งหมด", location: "/" },
+    { title: "วิทย์คอม", location: "/" },
+    { title: "วิศวะคอม", location: "/" },
+    { title: "ไอที", location: "/" },
+  ];
+
+  const books = [
+    { title: "ทั้งหมด", location: "/" },
+    { title: "คอร์สเรียนเสริม", location: "/" },
+    { title: "แบบฝึกหัด", location: "/" },
+  ];
+
+  const profiles = [
+    { title: "แก้ไขโปรไฟล์", icon: "edit-1", location: "/member" },
+    { title: "รายการที่ฉันสนใจ", icon: "edit-2", location: "/" },
+    { title: "ออกจากระบบ", icon: "logout", location: "/" },
+  ];
+
+  useEffect(() => {
+    let handler = (e) => {
+      if (!menuRef.current.contains(e.target)) {
+        setShowDropdown("");
+      }
+    };
+
+    document.addEventListener("mousedown", handler);
+  });
+
+  const openDropdown = (key) => {
+    if (showDropdown == key) {
+      return setShowDropdown("");
+    }
+    return setShowDropdown(key);
+  };
+
   return (
-    <div className="flex justify-between items-center py-2.5 px-[104px] bg-green">
+    <div
+      className="flex justify-between items-center py-2.5 px-[104px] bg-green"
+      ref={menuRef}
+    >
       <div className="flex items-center">
-        <div className="flex items-center gap-1.5 mr-16">
-          <img
-            src="/assets/logo.svg"
-            alt="logo"
-            className="py-1.5"
-            width="24"
-            height="24"
-          />
-          <div className="text-xl font-semibold">Comport</div>
-        </div>
+        <Link href="/member">
+          <div className="flex items-center gap-1.5 mr-16">
+            <img
+              src="/assets/logo.svg"
+              alt="logo"
+              className="py-1.5"
+              width="24"
+              height="24"
+            />
+            <div className="text-xl font-semibold">Comport</div>
+          </div>
+        </Link>
         <div className="flex items-center divide-x-[1px] divide-[#ABCCB6]">
           {menu.map((item, index) => {
-            return (
-              <button className="flex items-center px-3" key={item.icon}>
-                <img
-                  src={`/assets/icons/${item.icon}.svg`}
-                  alt={`icon-${item.icon}`}
-                  className="mr-1.5"
-                  width="16"
-                  height="16"
-                />
-                <div className="font-semibold">{item.title}</div>
-              </button>
+            return index == 0 ? (
+              <Link href={item.location}>
+                <button className="flex items-center px-3" key={item.icon}>
+                  <img
+                    src={`/assets/icons/${item.icon}.svg`}
+                    alt={`icon-${item.icon}`}
+                    className="mr-1.5"
+                    width="16"
+                    height="16"
+                  />
+                  <div className="font-semibold">{item.title}</div>
+                </button>
+              </Link>
+            ) : (
+              <div className="relative">
+                <button
+                  className="flex items-center px-3"
+                  key={item.icon}
+                  onClick={() => openDropdown(item.icon)}
+                >
+                  <img
+                    src={`/assets/icons/${item.icon}.svg`}
+                    alt={`icon-${item.icon}`}
+                    className="mr-1.5"
+                    width="16"
+                    height="16"
+                  />
+                  <div className="font-semibold">{item.title}</div>
+                </button>
+                {item.icon == "advertise" && showDropdown == "advertise" && (
+                  <div className={`absolute top-11 left-0 z-20 bg-green`}>
+                    {advertises.map((value, i) => {
+                      return (
+                        <Link href={value.location} key={`advertise-${i}`}>
+                          <div className={`pl-3 pr-9 py-3`}>
+                            • {value.title}
+                          </div>
+                          <hr
+                            className={`opacity-20 ${
+                              i == advertises.length - 1 && "hidden"
+                            }`}
+                          />
+                        </Link>
+                      );
+                    })}
+                  </div>
+                )}
+                {item.icon == "advice" && showDropdown == "advice" && (
+                  <div className={`absolute top-11 left-0 z-20 bg-green`}>
+                    {advices.map((value, i) => {
+                      return (
+                        <Link href={value.location} key={`advices-${i}`}>
+                          <div className={`pl-3 pr-8 py-3`}>
+                            • {value.title}
+                          </div>
+                          <hr
+                            className={`opacity-20 ${
+                              i == advertises.length - 1 && "hidden"
+                            }`}
+                          />
+                        </Link>
+                      );
+                    })}
+                  </div>
+                )}
+                {item.icon == "book" && showDropdown == "book" && (
+                  <div
+                    className={`w-[150px] absolute top-11 left-0 z-20 bg-green`}
+                  >
+                    {books.map((value, i) => {
+                      return (
+                        <Link href={value.location} key={`books-${i}`}>
+                          <div className={`pl-3 pr-3 py-3`}>
+                            • {value.title}
+                          </div>
+                          <hr
+                            className={`opacity-20 ${
+                              i == advertises.length - 1 && "hidden"
+                            }`}
+                          />
+                        </Link>
+                      );
+                    })}
+                  </div>
+                )}
+              </div>
             );
           })}
         </div>
       </div>
       <div className="flex items-center">
         <div className="flex items-center mr-[22px]">
-          <div className="flex gap-2.5 mr-12">
+          <div
+            className="flex gap-2.5 mr-12 relative cursor-pointer"
+            onClick={() => openDropdown("profile")}
+          >
             <img
               className="rounded-full"
               src="/assets/images/mock-profile-image.png"
@@ -52,6 +181,41 @@ const Header = () => {
               src="/assets/icons/arrow-point-to-down.svg"
               alt="arrow-profile"
             />
+            <div
+              className={`w-[200px] absolute top-[51px] left-0 z-20 bg-green`}
+            >
+              {showDropdown == "profile" &&
+                profiles.map((value, index) => {
+                  return index < profiles.length - 1 ? (
+                    <Link href={value.location} key={`profiles-${index}`}>
+                      <div className={`flex pl-3 py-3`}>
+                        <img
+                          src={`/assets/icons/${value.icon}.svg`}
+                          alt={`icon-${value.icon}`}
+                          className="mr-1.5"
+                          width="16"
+                          height="16"
+                        />
+                        {value.title}
+                      </div>
+                    </Link>
+                  ) : (
+                    <div>
+                      <hr className={`opacity-20`} />
+                      <div className={`flex pl-3 py-3`}>
+                        <img
+                          src={`/assets/icons/${value.icon}.svg`}
+                          alt={`icon-${value.icon}`}
+                          className="mr-1.5"
+                          width="16"
+                          height="16"
+                        />
+                        {value.title}
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
           </div>
           <div>
             <img
--- pages/create-portfolio/certificate/createCertificate.js
+++ pages/create-portfolio/certificate/createCertificate.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Breadcrumb from "@components/Breadcrumb";
 import StateCreate from "@components/StateCreate";
 import MoveToTop from "@components/MoveToTop";
 import Link from "next/link";
+import axios from "axios";
 
 const CreateCertificate = () => {
   const coverImage = "/assets/images/portfolio/portfolio-4-7.png";
@@ -16,6 +17,180 @@ const CreateCertificate = () => {
       link: "/create-portfolio",
     },
   ];
+  const [page, setPage] = useState(4);
+  const [certificates, setCertificates] = useState([
+    {
+      id: -1,
+      title: "",
+      file: "",
+    },
+    {
+      id: -1,
+      title: "",
+      file: "",
+    },
+    {
+      id: -1,
+      title: "",
+      file: "",
+    },
+  ]);
+
+  const newCertificates = () => {
+    setCertificates([
+      {
+        id: -1,
+        title: "",
+        file: "",
+      },
+      {
+        id: -1,
+        title: "",
+        file: "",
+      },
+      {
+        id: -1,
+        title: "",
+        file: "",
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    const idPort8 = localStorage.getItem("idPort8") || null;
+    const idFilePort8 = localStorage.getItem("idFilePort8") || null;
+
+    if (idPort8 && idFilePort8) {
+      getPort(idPort8.split(","), idFilePort8.split(","));
+    }
+  }, []);
+
+  const getPort = async (idPorts, idFilePorts) => {
+    let arr = [];
+    idPorts.forEach(async (idport, index) => {
+      await axios
+        .get(`http://localhost:1337/api/port-step-8s/${idport}`)
+        .then(async (data) => {
+          if (data.status === 200) {
+            if (page == 3) {
+              setPage(data.data.data.attributes.numberPage);
+            }
+            let model = {
+              id: data.data.data.id,
+              title: data.data.data.attributes.title,
+            };
+
+            await axios
+              .get(
+                `http://localhost:1337/api/upload/files/${idFilePorts[index]}`
+              )
+              .then((data) => {
+                if (data.status === 200) {
+                  model.file = data.data.data.attributes.cerImage;
+                }
+              })
+              .catch((e) => {
+                console.log(e);
+              });
+
+            arr.push(model);
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    });
+    if (arr.length > 0) {
+      setCertificates(arr);
+    }
+  };
+
+  const onPreviewImage = (e, index) => {
+    if (e.target.files.length > 0) {
+      let arr = [...certificates];
+      const file = e.target.files[0];
+
+      if (file) {
+        arr[index].file = file;
+      }
+      setCertificates([...arr]);
+    }
+  };
+
+  const onSubmit = async () => {
+    const idPort = localStorage.getItem("idPort") || null;
+    const idPort8 = localStorage.getItem("idPort8") || null;
+
+    let arrIdPort8 = [];
+    let arrIdFilePort8 = [];
+
+    if (idPort8) {
+      arrIdFilePort8 = idPort8.split(",");
+    }
+
+    if (idPort) {
+      let model = {
+        data: {
+          numberPage: page,
+          iduser: 1,
+          idPort: idPort,
+        },
+      };
+
+      let arr = [...certificates];
+
+      arr.forEach(async (cer) => {
+        model.data.title = cer.title;
+        if (cer.id >= 0) {
+          await axios
+            .put(`http://localhost:1337/api/port-step-8s/${cer.id}`, model)
+            .then((data) => {
+              if (data.status === 200) {
+                console.log("update port step 8 id => ", data.data.data.id);
+              }
+            })
+            .catch((e) => {
+              console.log(e);
+            });
+        } else {
+          await axios
+            .post("http://localhost:1337/api/port-step-8s", model)
+            .then((data) => {
+              if (data.status === 200) {
+                arrIdPort8.push(data.data.data.id);
+                cer.id = data.data.data.id;
+              }
+            })
+            .catch((e) => {
+              console.log(e);
+            });
+        }
+
+        if (cer.id >= 0 && cer.file) {
+          const form = new FormData();
+          form.append("files", cer.file);
+          form.append("ref", "api::port-step-8.port-step-8");
+          form.append("refId", cer.id);
+          form.append("field", "cerImage");
+          await axios
+            .post(`http://localhost:1337/api/upload`, form)
+            .then((data) => {
+              if (data.status === 200) {
+                arrIdFilePort8.push(data.data.data.id);
+              }
+            });
+        }
+      });
+
+      if (arrIdPort8.length > 0) {
+        localStorage.setItem("idPort8", arrIdPort8);
+      }
+      if (arrIdFilePort8.length > 0) {
+        localStorage.setItem("idFilePort8", arrIdFilePort8);
+      }
+    }
+  };
+
   return (
     <div className="px-[104px] py-[29px]">
       <MoveToTop />
@@ -59,6 +234,8 @@ const CreateCertificate = () => {
                 name="page_number"
                 className="shadow border rounded py-2.5 px-3.5 w-[531px]"
                 defaultValue="4"
+                value={page}
+                onChange={(e) => setPage(e.target.value)}
               >
                 <option value="1">1</option>
                 <option value="2">2</option>
@@ -66,106 +243,75 @@ const CreateCertificate = () => {
                 <option value="4">4</option>
               </select>
             </div>
-            <hr className="border-[1px] border-black opacity-25 w-full my-[42px]" />
-            <div className="flex mb-6">
-              <div className="pl-[250px]">
-                <div className="flex justify-center items-center w-[300px] h-[200px] rounded-[11px] bg-[#FFEC85]">
-                  <img
-                    src="/assets/icons/camera-icon.svg"
-                    alt="/assets/icons/camera-icon.svg"
-                    width={56}
-                    height={56}
-                  />
-                </div>
-              </div>
-              <div className="ml-[30px]">
-                <div className="font-bold mb-[13px]">คำแนะนำ</div>
-                <div className="mb-[9px]">• สำหรับรูปโปรไฟล์</div>
-                <div className="mb-[28px]">
-                  • ขนาดรูปไม่เกิน 10 MB png หรือ jpg
-                </div>
-                <button className="py-3 px-[42px] border-[1px] rounded-full text-lg font-bold">
-                  อัพโหลดรูป
-                </button>
-              </div>
-            </div>
-            <div className="flex items-center mb-3">
-              <label className="w-[218px] text-lg font-bold mr-8 text-end">
-                ชื่อเกียรติบัตร/งานอบรม 1
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-[531px] py-[6px] px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-              />
-            </div>
-            <hr className="border-[1px] border-black opacity-25 w-full my-[42px]" />
-            <div className="flex mb-6">
-              <div className="pl-[250px]">
-                <div className="flex justify-center items-center w-[300px] h-[200px] rounded-[11px] bg-[#FFEC85]">
-                  <img
-                    src="/assets/icons/camera-icon.svg"
-                    alt="/assets/icons/camera-icon.svg"
-                    width={56}
-                    height={56}
-                  />
-                </div>
-              </div>
-              <div className="ml-[30px]">
-                <div className="font-bold mb-[13px]">คำแนะนำ</div>
-                <div className="mb-[9px]">• สำหรับรูปโปรไฟล์</div>
-                <div className="mb-[28px]">
-                  • ขนาดรูปไม่เกิน 10 MB png หรือ jpg
-                </div>
-                <button className="py-3 px-[42px] border-[1px] rounded-full text-lg font-bold">
-                  อัพโหลดรูป
-                </button>
-              </div>
-            </div>
-            <div className="flex items-center mb-3">
-              <label className="w-[218px] text-lg font-bold mr-8 text-end">
-                ชื่อเกียรติบัตร/งานอบรม 2
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-[531px] py-[6px] px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-              />
-            </div>
-            <hr className="border-[1px] border-black opacity-25 w-full my-[42px]" />
-            <div className="flex mb-6">
-              <div className="pl-[250px]">
-                <div className="flex justify-center items-center w-[300px] h-[200px] rounded-[11px] bg-[#FFEC85]">
-                  <img
-                    src="/assets/icons/camera-icon.svg"
-                    alt="/assets/icons/camera-icon.svg"
-                    width={56}
-                    height={56}
-                  />
-                </div>
-              </div>
-              <div className="ml-[30px]">
-                <div className="font-bold mb-[13px]">คำแนะนำ</div>
-                <div className="mb-[9px]">• สำหรับรูปโปรไฟล์</div>
-                <div className="mb-[28px]">
-                  • ขนาดรูปไม่เกิน 10 MB png หรือ jpg
-                </div>
-                <button className="py-3 px-[42px] border-[1px] rounded-full text-lg font-bold">
-                  อัพโหลดรูป
-                </button>
-              </div>
-            </div>
-            <div className="flex items-center mb-3">
-              <label className="w-[218px] text-lg font-bold mr-8 text-end">
-                ชื่อเกียรติบัตร/งานอบรม 3
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-[531px] py-[6px] px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                type="text"
-              />
-            </div>
+            {certificates.map((item, index) => {
+              return (
+                <>
+                  <hr className="border-[1px] border-black opacity-25 w-full my-[42px]" />
+                  <div className="flex mb-6">
+                    <div className="pl-[250px]">
+                      {item.file == "" ? (
+                        <div className="flex justify-center items-center w-[300px] h-[200px] rounded-[11px] bg-[#FFEC85]">
+                          <img
+                            src="/assets/icons/camera-icon.svg"
+                            alt="/assets/icons/camera-icon.svg"
+                            width={56}
+                            height={56}
+                          />
+                        </div>
+                      ) : (
+                        <img
+                          src={URL.createObjectURL(item.file)}
+                          alt="preview-image"
+                          className="w-[300px] h-[200px] rounded-[11px] object-cover border"
+                        />
+                      )}
+                    </div>
+                    <div className="ml-[30px]">
+                      <div className="font-bold mb-[13px]">คำแนะนำ</div>
+                      <div className="mb-[9px]">• สำหรับรูปโปรไฟล์</div>
+                      <div className="mb-[28px]">
+                        • ขนาดรูปไม่เกิน 10 MB png หรือ jpg
+                      </div>
+                      <label
+                        htmlFor={`dropzone-file-${index}`}
+                        className="py-3 px-[42px] border-[1px] rounded-full text-lg font-bold"
+                      >
+                        อัพโหลดรูป
+                      </label>
+                      <input
+                        id={`dropzone-file-${index}`}
+                        type="file"
+                        className="hidden"
+                        accept="image/*"
+                        onChange={(e) => onPreviewImage(e, index)}
+                      />
+                    </div>
+                  </div>
+                  <div className="flex items-center mb-3">
+                    <label className="w-[218px] text-lg font-bold mr-8 text-end">
+                      ชื่อเกียรติบัตร/งานอบรม {index + 1}
+                    </label>
+                    <input
+                      className="shadow appearance-none border rounded w-[531px] py-[6px] px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      type="text"
+                      value={item.title}
+                      onChange={(e) => {
+                        let arr = [...certificates];
+                        arr[index].title = e.target.value;
+                        setCertificates(arr);
+                      }}
+                    />
+                  </div>
+                </>
+              );
+            })}
             <div className="flex items-center mt-[88px]">
               <div className="w-[218px] mr-8" />
               <Link href="/create-portfolio/certificate/createCertificate">
-                <button className="flex gap-1.5 items-center text-white font-bold bg-black rounded-full py-[13px] px-[22px]">
+                <button
+                  className="flex gap-1.5 items-center text-white font-bold bg-black rounded-full py-[13px] px-[22px]"
+                  onClick={newCertificates}
+                >
                   <img
                     src="/assets/icons/plus-white.svg"
                     alt="plus"
@@ -182,7 +328,10 @@ const CreateCertificate = () => {
       <hr className="border-gray-4 mb-4" />
       <div className="flex justify-center items-center">
         <Link href="/create-portfolio/download">
-          <button className="flex items-center bg-[#D9D9D9] px-5 py-2.5 rounded-[20px]">
+          <button
+            className="flex items-center bg-[#D9D9D9] px-5 py-2.5 rounded-[20px]"
+            onClick={onSubmit}
+          >
             บันทึกข้อมูล
           </button>
         </Link>
